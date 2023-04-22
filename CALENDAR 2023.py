8db6c3f67dc91d6ceb3f319058b356738bd221d9
ADEDEJI ADESOLA <114597199+Adedeji198@users.noreply.github.com>
2023-04-22 06:37:27 +0100

    Add files via upload

---
+++ CALENDAR 2023.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## Calendar
+
+# In[1]:
+
+
+from calendar import calendar
+year=2023
+num_of_column=4
+print(calendar(theyear=year, m=num_of_column))
+
+
+# In[9]:
+
+
+from calendar import calendar
+year=2023
+num_of_column=3
+print(calendar(theyear=year, m=num_of_column))
+
+
+# In[ ]:
+
+
+
+
